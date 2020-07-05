bf5602265d9cc2bde9514ed013b0bbf27ee8726a
diksha002 <diksha.raghoonundun@gmail.com>
2020-07-05 14:48:12 +0400

    [diksha] avec requete fichier

--- server.c
+++ server.c
@@ -5,10 +5,59 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include<unistd.h>
+#include <dirent.h>
 #define MAX 80
 #define PORT 8080
 #define SA struct sockaddr
 
+//function to list all the files on the server
+void list(int sockfd)
+{
+	DIR *d;
+
+	struct dirent *dir;
+
+	d = opendir("/home/ubuntu/td");
+
+	if (d)
+	{
+		while ((dir = readdir(d)) != NULL)
+		{
+			printf("%s\n", dir->d_name);
+		}
+
+		closedir(d);
+
+	}
+}
+
+//function to send file
+void sentfile(int sockfd)
+{
+	char buff[MAX];       // for read operation from file and used to sent operation 
+ 
+	// create file 
+	FILE *fp;
+	char x;
+	printf("which file to send: ");
+	scanf("%s", &x);
+	fp=fopen(&x,"r");  // open file uses both stdio and stdin header files
+	
+	// file should be present at the program directory
+	if( fp == NULL ){
+	printf("Error IN Opening File .. \n");
+	return ;
+	}
+ 
+	while ( fgets(buff,MAX,fp) != NULL ) // fgets reads upto MAX character or EOF 
+	write(sockfd,buff,sizeof(buff));  // sent the file data to stream
+ 
+	fclose (fp);       // close the file 
+ 
+	printf("File Sent successfully !!! \n");
+ 
+}
 //chat between client and server.
 void func(int sockfd)
 {
@@ -37,6 +86,17 @@ void func(int sockfd)
 			printf("Server Exit...\n");
 			break;
 		}
+	
+		//for file transfer purposes
+		if (strncmp("send", buff, 4) == 0){
+			printf("sending file....\n");
+			sentfile(sockfd);
+			break;
+		}
+		
+		if (strncmp("list", buff, 4) == 0){
+			break;
+		}
 	}
 }
 
@@ -45,6 +105,7 @@ int main()
 {
 	int sockfd, connfd, len;
 	struct sockaddr_in servaddr, cli;
+	char buff[MAX];
 
 	//socket create and verification
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
